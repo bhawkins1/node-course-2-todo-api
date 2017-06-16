31f761ecd7e99389df99586753202d254c638642
Ben Hawkins <Ben.Hawkins@JohnLewis.co.uk>
2017-06-16 15:20:15 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -10,11 +10,11 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-var todo = new Todo({
-  text: req.body.text
-});
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-  todo.save().then((doc ) => {
+  todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
@@ -23,15 +23,34 @@ var todo = new Todo({
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
+
+
+app.get('/todos/:id', (req, res) => {
+ var id = req.params.id;
+
+ if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+      }
+
+      Todo.findById(id).then((todo) => {
+        if (!todo) {
+          return res.status(404).send();
+        }
+
+res.send({todo});
+      }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(3000, () => {
-  console.log('started on port 3000')
+  console.log('Started on port 3000');
 });
-
 
 module.exports = {app};
